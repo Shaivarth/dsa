93a072acce70c9e7794de044ef76e8874207d8d1
Shaivarth <sarthakmishra080803@gmail.com>
2026-05-19 12:22:46 +0530

    intersection

---
+++ Arrays/easy/intersection_of_twoarrays.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> freq;
+        vector<int> result;
+        for (int num : nums1) {
+            freq[num]++;
+        }
+        for (int num : nums2) {
+            if (freq[num] > 0) {
+                result.push_back(num);
+                freq[num]--;
+            }
+        }
+        return result;
+    }
+};
+int main() {
+    vector<int> nums1 = {1, 2, 2, 2, 1, 3};
+    vector<int> nums2 = {2, 3};
+    Solution s;
+    vector<int> ans = s.intersect(nums1, nums2);
+    cout << "Intersection Array: ";
+    for (int num : ans) {
+        cout << num << " ";
+    }
+    return 0;
+}
