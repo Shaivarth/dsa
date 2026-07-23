0f49c9dac046fd24084bbbc8ad8788e167773370
Shaivarth <sarthakmishra080803@gmail.com>
2026-07-23 15:22:14 +0530

    53. Maximum Subarray

---
+++ Arrays/easy/53.py
@@ -0,0 +1,9 @@
+# I've solved this problem using Kadane's Algorithm.
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        currentSum = nums[0]
+        maxSum = nums[0]
+        for i in range(1, len(nums)):
+            currentSum = max(nums[i], currentSum + nums[i])
+            maxSum = max(maxSum, currentSum)
+        return maxSum
